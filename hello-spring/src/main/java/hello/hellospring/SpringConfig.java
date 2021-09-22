906d3a5c44d38fe54565301f18f4975eb1a48bb6
jungyeeun00 <ekfqlc0103@naver.com>
2021-09-23 02:55:54 +0900

    section 4

---
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -0,0 +1,20 @@
+package hello.hellospring;
+
+import hello.hellospring.domain.MemberRepository;
+import hello.hellospring.domain.MemoryMemberRepository;
+import hello.hellospring.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+    @Bean
+    public MemberService memberService(){
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
+}
